3970f1d05d5d5a14022eb78e9e7a3205136fe840
Aditya Naik <naik.aditya@gmail.com>
2019-01-12 11:15:50 +0100

    adds basic and assertion step files

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,23 @@
+Given("the following user exists") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("I visit the site") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("I click {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  
+  
