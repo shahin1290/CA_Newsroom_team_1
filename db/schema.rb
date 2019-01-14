a2c6ef9e4b42ca60b561b28acff25d4823daa9d2
camron <camronmoradi@gmail.com>
2019-01-14 12:19:26 +0100

    Updates Article model in db

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2019_01_12_111536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "lede"
+    t.text "body"
+    t.string "author"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,15 +37,6 @@ ActiveRecord::Schema.define(version: 2019_01_12_111536) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "lede"
-    t.text "body"
-    t.string "author"
-    t.date "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
