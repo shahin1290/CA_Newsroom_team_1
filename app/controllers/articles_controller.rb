69a0a18a43faf0964ea980f2aed9cb1e1768a187
shahin1290 <s.patowary@yahoo.com>
2019-01-15 13:12:05 +0100

    adds functionality to upload image

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :lede, :body, :author))
+        @article = Article.new(params.require(:article).permit(:title, :lede, :body, :author, :image))
  
         if @article.save
             redirect_to articles_path, notice: "Article is successfully created."
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 
   validates_presence_of  :title, :lede, :body, :author
+  has_one_attached :image
 
 end
