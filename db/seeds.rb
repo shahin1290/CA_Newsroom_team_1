09a0a7ac1c53136fe76102dd12b2dc5794486c6f
Aditya Naik <naik.aditya@gmail.com>
2019-01-15 10:59:38 +0100

    Updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 5.times do
-    Article.create(title: "Title", lede: "lede", body:"body", author:"author", date:"2018-12-12")
+    Article.create(title: "Title", lede: "lede", body:"body", author:"author")
 end
+
+User.create(email: "member@mail.com", password: "membermember", first_name: "Member", last_name: "Member", role: 0)
+User.create(email: "journalist@mail.com", password: "journalist", first_name: "Journo", last_name: "Journo", role: 1)
