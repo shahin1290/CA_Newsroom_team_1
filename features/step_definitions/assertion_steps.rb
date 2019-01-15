d27cdd5f251d90d1127ff812befbac47683b84c3
camron <camronmoradi@gmail.com>
2019-01-15 09:53:52 +0100

    Creates feature file and adds basic steps

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -5,3 +5,11 @@ end
 Then("I should not see {string}") do |content|
   expect(page).to have_no_content content
 end
+
+Then("I am on Create article page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on index page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
