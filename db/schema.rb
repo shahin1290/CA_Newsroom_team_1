6879fb6f7f4ccca2026d46b5ff58e9ad8a84fff7
Aditya Naik <naik.aditya@gmail.com>
2019-01-15 13:23:15 +0100

    Adds category model, adds unit tests, adds validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_090605) do
+ActiveRecord::Schema.define(version: 2019_01_15_122008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2019_01_15_090605) do
     t.text "lede"
     t.text "body"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
