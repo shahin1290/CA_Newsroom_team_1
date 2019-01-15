550b712e0e4434ad036d0a00fad97adcd816564a
camron <camronmoradi@gmail.com>
2019-01-15 09:46:24 +0100

    Removes date column from Articles db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_111536) do
+ActiveRecord::Schema.define(version: 2019_01_15_084431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_01_12_111536) do
     t.text "lede"
     t.text "body"
     t.string "author"
-    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -35,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_01_12_111536) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
