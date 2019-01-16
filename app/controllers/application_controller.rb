393813a4d7e1805fbf546f7aa148654b6d92397e
shahin1290 <s.patowary@yahoo.com>
2019-01-16 21:02:06 +0100

    adds link_to_unless_current in layout and modifies views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?, :set_locale
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
   def configure_permitted_parameters
     attributes = [:first_name, :last_name]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
