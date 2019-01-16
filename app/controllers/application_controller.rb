c26d40f7d2b5193b16b8385bca742a25dec81cf7
Aditya Naik <naik.aditya@gmail.com>
2019-01-16 20:40:38 +0100

    Adds sv.yml, updates application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?, :set_locale
 
   def configure_permitted_parameters
     attributes = [:first_name, :last_name]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
   end
+end
+
+def set_locale
+  I18n.locale = params[:locale] || I18n.default_locale
 end
