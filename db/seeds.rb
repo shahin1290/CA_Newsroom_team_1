5b81889c7c7e951dfe5341bdce510a31dc7ad83e
shahin1290 <s.patowary@yahoo.com>
2019-01-19 00:09:27 +0100

    Modifies index to show articles for the current user only

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
-5.times do
-    Article.create(title: "Title", lede: "Lorem ipsum dolor sit amet.", body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", author:"author", category_id: 1)
-end
-
 User.create(email: "member@mail.com", password: "password", first_name: "Member", last_name: "Member", role: 0)
 User.create(email: "journalist@mail.com", password: "password", first_name: "Journo", last_name: "Journo", role: 1)
+User.create(email: 'shahin@mail.com', first_name: "Journo", last_name: "Journo", password: 'password', role: 1)
+User.create(email: 'editor@mail.com', first_name: "Journo", last_name: "Journo", password: 'password', role: 1)
 
 categories = ["Sports", "Politics", "Health", "Finance", "Entertainment"]
 
 categories.each do |category| 
     Category.create(name: category)
 end
+
+Article.create(title: "Title", lede: "Lorem ipsum dolor sit amet.", body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", author:"author", category_id: 1, user_id:1)
+Article.create(title: "Title", lede: "Lorem ipsum dolor sit amet.", body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", author:"author", category_id: 2, user_id:1)
+Article.create(title: "Title", lede: "Lorem ipsum dolor sit amet.", body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", author:"author", category_id: 1, user_id:1)
+Article.create(title: "Title", lede: "Lorem ipsum dolor sit amet.", body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", author:"author", category_id: 3, user_id:1)
+Article.create(title: "Title", lede: "Lorem ipsum dolor sit amet.", body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", author:"author", category_id: 1, user_id:0)
